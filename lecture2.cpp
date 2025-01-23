6b392cc10ca2fc061e1c0979cbf1583c8cf2a3f1
ADARSH KUMAR <155943727+itz-adarsh@users.noreply.github.com>
2025-01-23 10:30:26 +0530

    Add files via upload

---
+++ lecture2.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+int main(){
+//    cout << "Namste Bharat"<<endl;
+//    cout << "Enter a number"<<endl;
+//    int a;
+//    cin>>a;
+//    cout<<"Number is :"<<a<<endl;
+//    int a=5;
+//    cout <<"number is : "<<a<<endl;
+//    
+//    char ch = 'a';
+//    cout<<"value of ch is : "<<ch<<endl;
+//    
+//    bool flag = true;
+//    cout<<"value of flag is : "<<flag<<endl;
+    
+    //to check the size taken by datatypes
+    
+//    int a = 5;
+//    cout<<sizeof(a)<<endl;
+//    
+//    short b = 5;
+//    cout<<sizeof(b)<<endl; 
+//    
+//    bool c = 5;
+//    cout<<sizeof(c)<<endl;
+    
+//    Implicit typecasting - autmatically converts one data type to another
+      
+//      char ch = 98;
+//      cout<< ch <<endl;
+//      
+//      int num = 'b';
+//      cout<< num<< endl;
+//      
+//      Explicit typecasting - manually cast by programmer to change from one datatype to another
+
+//    double d = 5.7;
+//    int a = int(d); // or we can use direct int x = d
+//    cout<<"value of a is :"<<a<<endl;
+//     
+//---------------Operator--------------------//
+     // (1) Arithmatic
+     
+	// int x = 5;
+	// int y = 3;
+	 
+//	 cout << x+y <<endl;
+//	 cout << x-y <<endl;
+//	 cout << x*y <<endl;
+//	 cout << x%y <<endl;
+//	 cout << x/y <<endl;
+//	 
+//	 cout<< 5.0/3 <<endl; 
+//	 cout<< 5.0/3.0 <<endl;
+//	 cout<< (float)5/3 << endl;
+	 
+	 
+	 // (2) Realtional opearator
+	 
+	 int a = 5;
+	 int b = 3;
+	 
+	 cout << (a>b) << endl;
+	 cout << (a<b) << endl;
+	 cout << (a!=b) << endl;
+	 cout << (a==b) << endl;
+	 cout << (a>=b) << endl;
+	 cout << (a<=b) << endl;
+}
+      
+     
+    
