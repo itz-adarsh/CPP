5d39bdb332cbb2d2f2bcf8cd5bd20d4e513ba4cc
ADARSH KUMAR <155943727+itz-adarsh@users.noreply.github.com>
2025-01-28 10:28:59 +0530

    Add files via upload

---
+++ condtional.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+// //----------- Example 1 -----------------  
+//    //declare a integer type variable
+//    int age;
+
+//    // print enter the age
+//    cout<<"Enter the age: ";
+
+//    // take input into age
+//       cin>>age;
+//    // if condtion
+//    if (age>=18){
+//     cout<<"I can vote"<<endl;
+//    }
+//    else{
+//     cout<<"I can't vote"<<endl;
+//    }
+// //-----------------------------------------
+
+//------------- Example 2 --------------------
+    //nested if-else
+   int marks;
+   cout <<"Enter the marks : ";
+   cin >> marks;
+
+   // if(marks>=90){
+   //    cout<<"A grade";
+   // }
+   // else{
+   //    if(marks>=80){
+   //       cout <<"B grade";
+   //    }
+   //    else{
+   //       if(marks>=60){
+   //          cout<<"C grade"; 
+   //       }
+   //       else{
+   //          if(marks>=40){
+   //             cout<<"D grade";
+   //          }
+   //          else{
+   //             cout<<"F grade";
+   //          }
+   //       }
+   //    }
+   // }
+
+ // if-elseif-else
+
+ if (marks>=90){
+   cout<<"A grade";
+ }
+ else if (marks>=80){
+    cout<<"B grade";
+ }
+ else if (marks>=60){
+   cout<<"C grade";
+ }
+ else if (marks>=40){
+   cout<<"D grade";
+ }
+ else{
+   cout<<"F grade";
+ }
+//---------------------------------------------
+
+//--------------- Example 3 -------------
+
+// int bronum;
+// cout<<"Enter the number of brothers : ";
+// cin>>bronum;
+
+// if (bronum==0){
+//    cout<<"Baat bn jayegi";
+// }
+// else{
+//    cout<<"Baat nhi banegi"; 
+// }
+
+}
