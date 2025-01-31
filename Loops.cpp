94b5c3b723e4ef424ef96bc572011617b6611d11
ADARSH KUMAR <155943727+itz-adarsh@users.noreply.github.com>
2025-01-31 23:13:33 +0530

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    
+
+    // for(int i=0; i<5; i=i+1){
+    //     cout<<"Adarsh Kumar"<<endl;
+    // }
+
+    // for(int i=0; i<5; i=i+1){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=5; i>0; i=i-1){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=1; i<=10; i=i+1){
+    //     cout<<2*i<<endl;
+    // }
+
+    // for(int i=0; i<=5; i=i+2){
+    //     cout<<i<<endl;
+    // }
+
+    // for (int i=1; i<=5; i=i+2){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=1; i<10; i=i*2){
+    //     cout<<i<<endl;
+    // }
+
+    // for (int i=100; i>0; i=i/2){
+    //     cout<<i<<endl;
+    // }
+
+    //  for (int i=5; (i>=0 && i<=10); i=i+1){
+    //     cout<<i<<endl;
+    //  }
+
+    int n;
+    if(cin>>n){
+        cout<<"Adarsh"<<endl;
+    }
+    
+    // if(cout<<"Adarsh"){
+    //     cout<<"Love";
+    // }
+
+    // int i = 0;
+    // for (; ; ){
+    //     if (i<5){      // also we can print using this
+    //         cout<<i<<endl;
+    //         i=i+1;
+    //     }
+    // }
+}
