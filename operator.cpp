5fae63f99c2fc9778589aac63f81254c81bcdaee
ADARSH KUMAR <155943727+itz-adarsh@users.noreply.github.com>
2025-02-01 10:45:18 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // ------------------ bitwise operator -----------------------
+    // bool a=false;
+    // bool b=true;
+    // cout<<(a&b)<<endl;;
+    // cout<<(a^b)<<endl;
+    // cout<<(a|b)<<endl;
+    // cout<<(~a)<<endl;
+    // cout<<(~b)<<endl;
+
+    // -------------------Left shift operator (<<)
+    // int a=-25;
+    // a= a<<1;
+    // cout<<a<<endl;
+    // //-------------------Right shift operator (>>)
+    // int b=-27;
+    // b= b>>1; 
+    // cout<<b;
+
+// pre(++a) and post(a++) increment
+
+    // int a=5;
+    // int c=++a+1;
+    // cout<<c<<endl;
+    
+// pre(--a) and post(a--) decrement
+
+    // int b=5;
+    // int d=--b +1;
+    // cout<<d<<endl;  
+
+    // int a=5;
+    // int b=6;
+    // cout<<(++a)+(--b);
+
+    // int a=5;
+    // cout<<(++a)*(++a)<<endl;
+    // cout<<(--a)*(--a)<<endl;
+    // cout<<(++a)*(--a)<<endl;
+    // cout<<(--a)*(++a)<<endl;
+    
+// ------------------ Break ans Continue keyword -------------------
+
+
+// Break - Exits thr loop entirely skipping all remaining iterations.
+
+    // int n;
+    // cin>>n;
+    // for(int i=0; i<n; i++){
+    //     cout<<"Adarsh"<<endl;
+    //    break; // yha pe ek baar print krke loop se directly exit ho jayega
+    // }
+    // cout<<"Kumar";
+
+    
+//    for(int i=0; i<n; i++){
+//         if(i==5){
+//             break; //exits the loop when i=5
+//         }
+//         cout<<i<<endl;
+//     }
+
+// Continue - skips the rest of the code for the current iterations and proceeds to the next iteration
+
+// for(int i=0; i<n; i++){
+//     continue;   // kuch print nhi krega directly sare iterations ko skip kr dega
+//     cout<<i<<endl;
+// }
+
+// for(int i=0; i<n; i++){
+//         if(i==5){
+//             continue; //skips the iteration when i=5
+//         }
+//         cout<<i<<endl;
+//     }
+
+// --------------------------- Variable scoping ------------------
+// variable scoping refers to the context within which a varaible is accessible or visible
+
+}
